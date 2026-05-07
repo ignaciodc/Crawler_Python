9379e892c909c230e623eb189d41033b6dd4c132
Hugo Benítez García <hbengar0801@g.educaand.es>
2026-05-07 13:11:18 +0200

    Refactor CSV export to a standalone function

--- src/crawler/result_exporter.py
+++ src/crawler/result_exporter.py
@@ -1,23 +1,17 @@
 import csv
 
-class ExportarResultados:
-    def __init__(self, archivo_salida):
-        self.archivo_salida = archivo_salida
-        
-"""recibe una lista con  datos de las páginas visitadas y los escribe en u archivo csv"""
+def exportar_csv(archivo_salida, datos_paginas):
+    """recibe una lista con datos de las páginas visitadas y los escribe en un archivo csv"""
     
-    def exportar_csv(self, datos_paginas):
+    # abrimos el archivo csv en modo escritura (o lo creamos)
+    with open(archivo_salida, mode='w', newline='', encoding='utf8') as archivo:
+        escritor_csv = csv.writer(archivo)
         
-        # abrimos el archivo csv en modo escritura (o lo creamos)
+        # Primera fila, cabeceras
+        escritor_csv.writerow(['URL', 'Titulo', 'Num de enlaces', 'ms (tiempo)'])
         
-        with open(self.archivo_salida, mode='w', newline='', encoding='utf8') as archivo:
-            escritor_csv = csv.writer(archivo)
-            
-            # Primera fila, cabeceras
-            escritor_csv.writerow(['URL', 'Titulo', 'Num de enlaces', 'ms (tiempo)'])
-            
-            # Recorre los datos y escribe una fila por pagina 
-            for pagina in datos_paginas:
-                escritor_csv.writerow([pagina['url'], pagina['titulo'], pagina['num_enlaces'], pagina['ms']])
+        # Recorre los datos y escribe una fila por pagina 
+        for pagina in datos_paginas:
+            escritor_csv.writerow([pagina['url'], pagina['titulo'], pagina['num_enlaces'], pagina['ms']])
 
-        print(f"Datos guardados en {self.archivo_salida}")
+    print(f"Datos guardados en {archivo_salida}")

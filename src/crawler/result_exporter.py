cb97d6b918a072094e1601fbca90272eb305934f
Hugo Benítez García <hbengar0801@g.educaand.es>
2026-05-07 11:39:48 +0200

    Refactor class name and add method docstring
    Renamed class from 'exportarResultados' to 'ExportarResultados' and added docstring for the 'exportar_csv' method.

--- src/crawler/result_exporter.py
+++ src/crawler/result_exporter.py
@@ -1,15 +1,22 @@
 import csv
 
-class exportarResultados:
+class ExportarResultados:
     def __init__(self, archivo_salida):
         self.archivo_salida = archivo_salida
-
+        
+"""recibe una lista con  datos de las páginas visitadas y los escribe en u archivo csv"""
+    
     def exportar_csv(self, datos_paginas):
+        
+        # abrimos el archivo csv en modo escritura (o lo creamos)
+        
         with open(self.archivo_salida, mode='w', newline='', encoding='utf8') as archivo:
             escritor_csv = csv.writer(archivo)
-
+            
+            # Primera fila, cabeceras
             escritor_csv.writerow(['URL', 'Titulo', 'Num de enlaces', 'ms (tiempo)'])
-
+            
+            # Recorre los datos y escribe una fila por pagina 
             for pagina in datos_paginas:
                 escritor_csv.writerow([pagina['url'], pagina['titulo'], pagina['num_enlaces'], pagina['ms']])
 

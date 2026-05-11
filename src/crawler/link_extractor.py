83bdddfd1cb04334a83ee7dc033e5fab6d6b41d9
HugoBenitezGarcia <hbenagr0801@iesrafaelalberti.es>
2026-05-11 11:19:04 +0200

    link_extractor

---
+++ src/crawler/link_extractor.py
@@ -0,0 +1,12 @@
+def extraer_enlaces (soup):
+    enlaces_validos = []
+    if soup is None:
+        return enlaces_validos
+    etiquetas = soup.select('a[href]')
+
+    for etiqueta in etiquetas:
+        enlace = etiqueta['href']
+
+        if enlace not in enlaces_validos:
+            enlaces_validos.append(enlace)
+    return enlaces_validos

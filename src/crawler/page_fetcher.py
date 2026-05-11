67eb98257d1750c5ed3134eef41e5454b7c8ee3b
dbauhue1708 <dbauhue1708@g.educaand.es>
2026-05-11 11:31:55 +0200

    añadido loger a page_fetcher.py y creado config_manager.py para parsear la configuración con los parámetros que se le manden en el config.ini

--- src/crawler/page_fetcher.py
+++ src/crawler/page_fetcher.py
@@ -1,44 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
-import logging
 
-# Configuramos el registro de problemas.
-# Se deben capturar y registrar errores de conexión o parseo[cite: 22].
-logging.basicConfig(level=logging.ERROR, format='%(levelname)s: %(message)s')
+from crawler.logger_util import configurar_logger
+
+logger = configurar_logger()
+
 
 def descargar_pagina(url):
     """
-    Descarga una página web.
-    Usa la URL indicada.
+    Descarga una página web usando la URL indicada.
     Devuelve el texto HTML.
     """
     try:
-        # Usar requests.get(url) para descargar la página[cite: 17].
+        # Usar requests.get(url) para descargar la página.
         respuesta = requests.get(url)
-        
+
         # Verificamos que no haya errores web (como un 404).
         respuesta.raise_for_status()
-        
-        # Devolvemos el código HTML en texto.
+
         return respuesta.text
-        
+
     except requests.RequestException as e:
-        # Anotamos el fallo si no hay internet o la web cae[cite: 22].
-        logging.error(f"Error al intentar descargar la URL {url}. Detalle: {e}")
+        # Capturamos y registramos errores de conexión.
+        logger.error(f"Error al intentar descargar la URL {url}. Detalle: {e}")
         return None
+
 
 def parsear_html(html):
     """
     Prepara el HTML para extraer datos.
     Convierte el texto en un objeto BeautifulSoup.
     """
-    # Si la descarga falló, cancelamos el proceso.
     if html is None:
         return None
-        
-    # Creamos el objeto para manipular documentos HTML[cite: 6].
-    # Usamos el motor 'html.parser' recomendado en los apuntes[cite: 18].
-    sopa = BeautifulSoup(html, 'html.parser')
-    
-    # Devolvemos la estructura lista para buscar enlaces.
-    return sopa
+
+    # Creamos el objeto para manipular documentos HTML.
+    try:
+        # Usamos el motor recomendado en los apuntes.
+        sopa = BeautifulSoup(html, 'html.parser')
+        return sopa
+
+    except Exception as e:
+        # También es buena idea capturar errores de parseo por si el HTML está muy roto
+        logger.error(f"Error al parsear el documento HTML. Detalle: {e}")
+        return None
